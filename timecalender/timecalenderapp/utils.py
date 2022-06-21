f16012e93ad05749ee3e4e08d3909a15d5961244
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-06-21 21:33:20 +0100

    feature: added utils for data clean up

---
+++ timecalender/timecalenderapp/utils.py
@@ -0,0 +1,12 @@
+
+# function cleans up the time data to be returned back as response
+def time_cleanup():
+    pass
+
+# function gets the holidays in each country provided
+def get_holidays(country):
+    pass
+
+# function gets overlapping time slots based on data provided
+def get_overlapping_time():
+    pass
--- timecalender/timecalenderapp/views.py
+++ timecalender/timecalenderapp/views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from serializers import TimeSlotSerializer
-from models import TimeSlots
-
+# from models import TimeSlots
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class TimeSlotsViewSet(viewsets.ModelViewSet):
     """
     API endpoint that receives the array of time hashes and handles the data appropriately.
     """
-    serializer_class = TimeSlots
+    serializer_class = TimeSlotSerializer
+
+
+    @action(detail=True, methods=['post'])
+    def process_overlapping_time(self, request, pk=None):
+        time_array_data = request.data
+        return time_array_data
