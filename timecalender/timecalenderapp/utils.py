74103eb9beb483c4044027ecd3d2da9fc4b54c85
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2022-06-22 23:26:03 +0100

    chore: worked on convert_to_utc method halfway

--- timecalender/timecalenderapp/utils.py
+++ timecalender/timecalenderapp/utils.py
@@ -1,7 +1,17 @@
+import pytz
+from datetime import datetime
 
-# function cleans up the time data to be returned back as response
-def time_cleanup():
-    pass
+# function converts time data to gmt
+def convert_to_utc(time_slot):
+    country_code = time_slot['CC']
+    from_time = time_slot['from']
+    to_time = time_slot['to']
+    timezone =  pytz.country_timezones[country_code]
+    dt_from = pytz.timezone(timezone[0]).localize(datetime.strptime(from_time, '%Y-%m-%d %H:%M:%S')).astimezone(pytz.utc)
+    dt_to = pytz.timezone(timezone[0]).localize(datetime.strptime(to_time, '%Y-%m-%d %H:%M:%S')).astimezone(pytz.utc)
+    
+    return {"utc":[country_code, dt_from, dt_to]}
+
 
 # function gets the holidays in each country provided
 def get_holidays(country):
--- timecalender/timecalenderapp/views.py
+++ timecalender/timecalenderapp/views.py
@@ -1,19 +1,27 @@
-from rest_framework import viewsets
-from rest_framework import permissions
 from .serializers import TimeSlotSerializer
 from .models import TimeSlots
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .utils import convert_to_utc
 
 class TimeSlotsApi(APIView):
     """
-    API endpoint that receives the array of time hashes and handles the data appropriately.
+    API view that receives the array of time hashes and handles the data appropriately.
     """
     permission_classes = [AllowAny]
 
-
+    # post request endpoint
     def post(self, request):
-        time_array_data = request.data
-        return time_array_data
+        # incoming request data
+        time_array_data = request.data.get('time_slots')
+        if not time_array_data:
+            return Response(dict(error="invalid time slot data provided"), status=status.HTTP_400_BAD_REQUEST)
+        # initialize empty array to store gmt conversions
+        gmt_time_array = []
+        for time_slot in time_array_data:
+            gmt_time = convert_to_utc(time_slot)
+            gmt_time_array.append(gmt_time)
+
+        return Response(dict(response=gmt_time_array), status=status.HTTP_200_OK)
